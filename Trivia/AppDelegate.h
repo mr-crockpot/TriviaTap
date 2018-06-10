799c867da447bcdb9e9358acdd763741be18b10a
mr-crockpot <adamschor@adams-macbook-air.local>
2018-06-09 22:47:47 -0600

    Initial Commit

---
+++ Trivia/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Trivia
+//
+//  Created by Adam Schor on 6/9/18.
+//  Copyright © 2018 AandA Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Trivia/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Trivia
+//
+//  Created by Adam Schor on 6/9/18.
+//  Copyright © 2018 AandA Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
